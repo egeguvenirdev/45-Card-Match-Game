f66f7a0fc20d0ff59f4cfc5051cf4391cd87c4d4
Ege Güvenir <egeguvenirdev@gmail.com>
2024-04-27 07:35:39 +0300

    Start Panel

--- Assets/Scripts/Utilities/ConstantVariables.cs
+++ Assets/Scripts/Utilities/ConstantVariables.cs
@@ -8,39 +8,4 @@ public static class ConstantVariables
     {
         public const string Level = nameof(Level);
     }
-
-    public static class TotalMoneyValue
-    {
-        public const string TotalMoney = nameof(TotalMoney);
-    }
-
-    public static class LevelStats
-    {
-        public const string Lv = nameof(Lv);
-        public const string SkillLevel = nameof(SkillLevel);
-    }
-
-    public static class UpgradeValues
-    {
-        public const string UpgradeCurrentValue = nameof(UpgradeCurrentValue);
-        public const string UpgradeIncrementalValue = nameof(UpgradeIncrementalValue);
-    }
-
-    public static class UpgradePrices
-    {
-        public const string CurrentPrice = nameof(CurrentPrice);
-        public const string IncrementalPrice = nameof(IncrementalPrice);
-    }
-
-    public static class UpgradeTypes
-    {
-        public const string Income = nameof(Income);
-        public const string FireRate = nameof(FireRate);
-        public const string FireRange = nameof(FireRange);
-    }
-
-    public static class BuyBallButton
-    {
-        public const string BuyButton = nameof(BuyButton);
-    }
 }
